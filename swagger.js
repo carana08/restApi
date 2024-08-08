76e07c5d89e5c1847eb39c05c95b76b31f830078
César Arana <carana@espol.edu.ec>
2024-08-08 18:02:04 -0500

    Actualizando proyecto

--- swagger.js
+++ swagger.js
@@ -6,9 +6,11 @@ const swaggerAutogen = require('swagger-autogen')()
      "description": 'REST API with Express and Firestore'
    },
    //"host": 'localhost:5500',
-   "host": 'restapi-o8ud.onrender.com',
+   "host": "restapi-o8ud.onrender.com",
    "basePath": "/api",
    "schemes": ['https'],
+  "consumes": ['application/json'],
+  "produces": ['application/json']
  };
 
  const outputFile = './swagger_output.json'

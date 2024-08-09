30e9d93536a37c01e0d627cffcbf1b7cee93a240
César Arana <carana@espol.edu.ec>
2024-08-08 20:17:49 -0500

    Actualiznado proyecto

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -2,24 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
 
-  /* 
-  #swagger.tags = ['Token']
-  #swagger.description = 'Validate token'
-  #swagger.summary = 'Validate token'
-  #swagger.parameters['token'] = {
-      in: 'header',
-      description: 'JWT token',
-      required: true,
-    }
-    #swagger.responses[200] = {
-        description: 'Token is valid',
-    }
-    #swagger.responses[408] = {
-        description: 'Token not provided or invalid',
-    }
-  */
-
-  const authHeader = req.headers['token']; // Cambiado de 'hola' a 'token'
+  const authHeader = req.headers.authorization;
+  
   console.log('Auth Header:', authHeader); // Registro adicional
   if (!authHeader) {
     return res.status(408).send('Token not provided');

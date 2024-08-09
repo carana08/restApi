565c28d175142ce9e2186b89b84c520c1c5a88c9
César Arana <carana@espol.edu.ec>
2024-08-08 19:17:15 -0500

    Modificando verifyJWT

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,36 +1,39 @@
 const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
+
   /* 
   #swagger.tags = ['Token']
   #swagger.description = 'Validate token'
   #swagger.summary = 'Validate token'
-  #swagger.parameters['Token'] = {
+  #swagger.parameters['token'] = {
       in: 'header',
       description: 'JWT token',
       required: true,
     }
     #swagger.responses[200] = {
-        description: '',
+        description: 'Token is valid',
     }
     #swagger.responses[408] = {
-        description: 'Token not provided',
+        description: 'Token not provided or invalid',
     }
-*/
+  */
 
-  const authHeader = req.headers['token'];
+  const authHeader = req.headers['token']; // Cambiado de 'hola' a 'token'
   if (!authHeader) {
     return res.status(408).send('Token not provided');
   }
 
   const token = authHeader;
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(408).send('Request Timeoutoken');
-    }
-    req.user = decoded;
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decodedToken;
     next();
-  });
+  } catch (error) {
+    console.error('Token verification error:', error);
+    res.status(408).send('Request Timeout. Invalid token.');
+  }
 };
+
 module.exports = verifyJWT;
